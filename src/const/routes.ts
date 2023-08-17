ac0e133d9d6255e16f564756dffe5efdea2463a4
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-17 22:07:14 +0300

    feat: add rotes

--- src/const/routes.ts
+++ src/const/routes.ts
@@ -1,2 +1,5 @@
 export const HeaderLinks = [{link: '/concerts', label: 'Концерты'}, {link: '/promocodes', label: 'Промокоды'}]
 export const ROUTES = { USER: 'user', ADMIN: 'admin'}
+export const LINKS = {
+    CONCERT: 'concert/'
+}
