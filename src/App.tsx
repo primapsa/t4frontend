cc82cd73eddd0299d8e48e6f0edcca6f824e763f
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-17 22:05:55 +0300

    feat: wrap app by router and mantine

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import {Container, MantineProvider} from "@mantine/core";
+import {HeaderSimple} from "./components/Header/HeaderMantine";
+import {appTheme, styles} from "./appTheme";
+import {HeaderLinks, ROUTES} from "./const/routes";
+import MainPage from "./features/Admin/MainPage";
+import Concert from "./components/Concert/Concert";
+import Alert from "./components/Alert/Alert";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={appTheme}>
+          <Alert/>
+          <BrowserRouter>
+              <Container sx={styles}>
+                  <HeaderSimple links={HeaderLinks}/>
+                  <Routes>
+                      <Route path={'/'} element={<MainPage/>}/>
+                      <Route path={`/concert/:id`} element={<Concert/>}/>
+                      {/*<Route path={`/${ROUTES.ADMIN}`} element={}/>*/}
+                  </Routes>
+              </Container>
+          </BrowserRouter>
+      </MantineProvider>
   );
 }
 
