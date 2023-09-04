d4e113ba631c6c0a5590f1f61f7a399f681e7656
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-04 23:12:57 +0300

    feat: add paypal utils fn

---
+++ src/utils/paypal.ts
@@ -0,0 +1,21 @@
+import {paypalAPI, PayPalResponseTYpe} from "../api/api";
+import {HTTP_STATUSES} from "../const/htttpStatus";
+import {MESSAGE} from "../const/messages";
+
+export const createPayOrder = (ids: number[]) => async (data: any, actions: any) => {
+    try {
+        const response = await paypalAPI.createOrder(ids)
+        if (response.status === HTTP_STATUSES.OK) {
+           return actions.order.create(response.data)
+        }
+        return MESSAGE.UNCAUGHT
+    } catch (error) {
+        return (error as Error).message
+    }
+}
+
+export const makePayPalMessage = (data: any) => {
+
+    return {title: MESSAGE.SUCCESS_PAYMENT.title,
+        message: `Заказ ${(data as PayPalResponseTYpe).orderID}\r\n${MESSAGE.SUCCESS_PAYMENT.message}`}
+}
