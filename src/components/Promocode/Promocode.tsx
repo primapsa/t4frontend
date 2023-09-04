3c3fcade9f3ab103eeb99601d4c2fa345650cf15
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-04 23:13:58 +0300

    feat: add promocode component

--- src/components/Promocode/Promocode.tsx
+++ src/components/Promocode/Promocode.tsx
@@ -1,64 +1,26 @@
-import React, {useState} from 'react';
-import {Button, Flex, Input, Badge} from '@mantine/core';
-import {IconPlus, IconX} from '@tabler/icons-react';
+import {Flex, Paper, Text} from "@mantine/core";
+import React from "react";
 import {useStyles} from "./style";
 
-
-const Promocode = ({promocode, id, addCallback}: PromocodePropsType) => {
+const Promocode = ({title, discount, date}: PromocodeItemType) => {
 
     const {classes} = useStyles()
-    const [isVisible, setIsVisible] = useState<boolean>(false)
-    const [value, setValue] = useState<string>(promocode || '')
-    const addHandler = () => {
-        addCallback(value, id)
-        setValue('')
-        setIsVisible(false)
-    }
-    const showHandler = () => {
-        setIsVisible(!isVisible)
-    }
-    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)
-    const buttonName = isVisible ? 'Закрыть' : 'Промокод'
-
 
     return (
-        <Flex className={classes.badge}>
-            {
-                !!promocode ?
-                    <Badge className={classes.badge} variant="outline" >
-                        {promocode}
-                    </Badge> :
-                    <Button variant="subtle" compact onClick={showHandler}>
-                        {buttonName}
-                    </Button>
-            }{
-            isVisible &&
-            <Flex>
-                <Input
-                    placeholder="Промокод"
-                    value={value}
-                    onChange={onChangeHandler}
-                    classNames={{input: classes.input}}
-                />
-                <Button className={classes.button}
-                        onClick={addHandler}
-                        compact size={'xs'}
-                        disabled={!value}
-                        variant={"outline"}>
-                    <IconPlus className={classes.icon}/>
-                </Button>
+        <Paper className={classes.promocode}>
+            <Flex className={classes.promocode__inner}>
+                <Text className={classes.promocode__title}>{title}</Text>
+                <Text>скидка {discount}%</Text>
             </Flex>
-        }
-        </Flex>
+            <Text>до {date}</Text>
+        </Paper>
+    )
+}
+export default React.memo(Promocode)
 
-    );
-};
-
-export default React.memo(Promocode);
-
-
-type PromocodePropsType = {
-    promocode: string | null
-    id: number
-    addCallback: (promocode: string, id: number) => void
+type PromocodeItemType = {
+    title: string
+    discount: number
+    date: string
+    editMode?: boolean
 }
