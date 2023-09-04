fa9d03de7a1e90d03fd13f8cdab9be53313b46cf
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-04 23:11:45 +0300

    feat: add emptystatewithloader component

---
+++ src/components/Empty/EmptyStateWithLoader.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {AppStatus} from "../../redux/appReducer";
+import {STATUS} from "../../const/statuses";
+import Preloader from "../Preloader/Preloader";
+import EmptyState from "./EmptyState";
+
+const EmptyStateWithLoader = ({isEmpty, children, status}: EmptyStateWithLoaderType) => {
+
+    if (status === STATUS.LOADING) {
+        return <Preloader/>
+    }
+
+    return (
+        <>
+            {
+                isEmpty ? <EmptyState/> : children
+            }
+        </>
+    );
+};
+
+export default React.memo(EmptyStateWithLoader);
+
+
+type EmptyStateWithLoaderType = {
+    isEmpty: boolean
+    status: AppStatus
+    children?: React.ReactNode
+}
