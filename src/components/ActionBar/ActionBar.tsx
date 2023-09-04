eb4c7e45225a45af3347900deafb1d7b79a14075
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-04 23:16:29 +0300

    reafactor: actionBar

--- src/components/ActionBar/ActionBar.tsx
+++ src/components/ActionBar/ActionBar.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import { ActionIcon, Flex } from '@mantine/core';
-import { IconTrash, IconEdit } from '@tabler/icons-react';
+import {ActionIcon, Flex} from '@mantine/core';
+import {IconTrash, IconEdit} from '@tabler/icons-react';
+import {useStyles} from "./styles";
+
 const ActionBar = ({del, edit, id}: ActionBarPropsType) => {
+
+    const {classes} = useStyles()
     const onDeleteHandler = del ? () => del(id) : null
     const onEditHandler = edit ? () => edit(id) : null
+
     return (
-        <Flex direction={'column'}>
-            {onDeleteHandler && <ActionIcon variant="outline" title="delete" color={'red'} onClick={onDeleteHandler}><IconTrash/></ActionIcon>}
-            {onEditHandler && <ActionIcon variant="outline" title="edit" color={'blue'} onClick={onEditHandler}><IconEdit/></ActionIcon>}
+        <Flex className={classes.bar}>
+            {onDeleteHandler &&
+                <ActionIcon className={classes.icon}
+                            variant="outline"
+                            title="delete"
+                            color={'red'}
+                            onClick={onDeleteHandler}>
+                    <IconTrash/>
+                </ActionIcon>}
+            {onEditHandler &&
+                <ActionIcon className={classes.icon}
+                            variant="outline"
+                            title="edit"
+                            color={'blue'}
+                            onClick={onEditHandler}>
+                    <IconEdit/>
+                </ActionIcon>}
         </Flex>
     );
 };
 
-export default ActionBar;
+export default React.memo(ActionBar);
 
 type ActionBarPropsType = {
     del?: (id: number) => void
