f83ab477dcd4ed8cf09f5f7d8969f2bbf3ef06a0
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-07 00:00:59 +0300

    feat: unused imports, selectors

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,32 +1,24 @@
 import React from 'react';
-import {Center, MantineProvider} from "@mantine/core";
 import {Navigate, Outlet} from "react-router-dom";
-import Preloader from "../Preloader/Preloader";
 import {LINKS} from "../../const/routes";
 import {useSelector} from "react-redux";
 import {RootStateType} from "../../redux/store";
-import {AppStatus} from "../../redux/appReducer";
-import {STATUS} from "../../const/statuses";
-import HeaderProps from "../Header/HeaderProps";
-import {AuthUserType} from "../../redux/authReducer";
 import HeaderUser from "../Header/HeaderUser";
+import {getIsAuth, getIsStaff} from "../../selectors/selectors";
 
 const PrivateRoute = () => {
-    const isAuth = useSelector<RootStateType, boolean>(state => state.auth.isAuth)
-    const isAdmin = useSelector<RootStateType, boolean>(state => state.auth.isStaff)
-    const user = useSelector<RootStateType, AuthUserType>(state => state.auth.user)
-    //const status = useSelector<RootStateType, AppStatus>(state => state.app.status)
 
-    // if (status === STATUS.LOADING)
-    //     return <Preloader/>
+    const isAuth = useSelector<RootStateType, boolean>(getIsAuth)
+    const isAdmin = useSelector<RootStateType, boolean>(getIsStaff)
 
-
+    if (isAdmin) {
+        return <Navigate to={'admin'}/>
+    }
     return (
         <>
             {
                 isAuth ?
                     <>
-                        { isAdmin &&  <Navigate to={LINKS.ADMIN}/>}
                         <HeaderUser/>
                         <Outlet/>
                     </> :
