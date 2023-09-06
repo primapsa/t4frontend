ba66c9108d9dc3ee827e072e5736af0238e62227
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-06 22:39:40 +0300

    refactor: styles out

--- src/components/404/NotFound.tsx
+++ src/components/404/NotFound.tsx
@@ -1,42 +1,6 @@
-import { createStyles, Title, Text, Button, Container, Group, rem } from '@mantine/core';
-
-const useStyles = createStyles((theme) => ({
-    root: {
-        paddingTop: rem(80),
-        paddingBottom: rem(80),
-    },
-
-    label: {
-        textAlign: 'center',
-        fontWeight: 900,
-        fontSize: rem(220),
-        lineHeight: 1,
-        marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
-
-        [theme.fn.smallerThan('sm')]: {
-            fontSize: rem(120),
-        },
-    },
-
-    title: {
-        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-        textAlign: 'center',
-        fontWeight: 900,
-        fontSize: rem(38),
-
-        [theme.fn.smallerThan('sm')]: {
-            fontSize: rem(32),
-        },
-    },
-
-    description: {
-        maxWidth: rem(500),
-        margin: 'auto',
-        marginTop: theme.spacing.xl,
-        marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
-    },
-}));
+import {Container, Group, Text, Title} from '@mantine/core';
+import {NavLink} from "react-router-dom";
+import {useStyles} from "./styles";
 
 export function NotFound() {
     const { classes } = useStyles();
@@ -50,9 +14,7 @@ export function NotFound() {
                 been moved to another URL.
             </Text>
             <Group position="center">
-                <Button variant="subtle" size="md">
-                    Take me back to home page
-                </Button>
+                <NavLink to={'/'}>На главную</NavLink>
             </Group>
         </Container>
     );
