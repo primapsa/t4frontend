e414d8f966a21808f5c611199dd913055e76673a
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-06 23:32:12 +0300

    refactor: selectors

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,18 +1,23 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {fetchConcertsTypes} from "../../redux/concertsReducer";
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatchType, RootStateType} from "../../redux/store";
 import {ConcertTypesType} from "../../api/api";
-import {Button, Flex, Input, Select} from '@mantine/core';
+import {Button, Input, Select} from '@mantine/core';
 import {IconSearch} from '@tabler/icons-react';
 import {useForm} from '@mantine/form';
 import {resetFilter, setFilter} from "../../redux/filterReducer";
+import {useStyles} from "./styles";
+import {getConcertType, getFilterQuery, getFilterType} from "../../selectors/selectors";
 
 const Filter = () => {
-    const types = useSelector<RootStateType, ConcertTypesType[]>(state => state.concerts.type)
-    const query = useSelector<RootStateType, string>(state => state.filter.query)
-    const type = useSelector<RootStateType, number>(state => state.filter.type)
+
+    const {classes} = useStyles()
+    const types = useSelector<RootStateType, ConcertTypesType[]>(getConcertType)
+    const query = useSelector<RootStateType, string>(getFilterQuery)
+    const type = useSelector<RootStateType, number>(getFilterType)
     const dispatch = useDispatch()
+
     const form = useForm({
         initialValues: {query, type},
     });
@@ -24,31 +29,33 @@ const Filter = () => {
     const formHandler = form.onSubmit((fields) => {
         dispatch<AppDispatchType>(setFilter(fields))
     })
-    const resetHandler = () => {
+    const resetHandler = useCallback(() => {
         dispatch<AppDispatchType>(resetFilter())
         form.reset()
-    }
+    }, [])
+
+
     return (
-        <Flex style={{padding: '20px'}}>
-            <form onSubmit={formHandler}>
-                <Input style={{minWidth: '250px'}}
-                       icon={<IconSearch/>}
-                       placeholder="Введите запрос"
-                       {...form.getInputProps('query')}
-                />
-                <Select
+        <form onSubmit={formHandler} className={classes.form}>
+            <Input className={classes.search}
+                   icon={<IconSearch/>}
+                   placeholder="Введите запрос"
+                   {...form.getInputProps('query')}
+            />
+            <Select className={classes.type}
                     placeholder="Выберите"
                     data={types}
                     {...form.getInputProps('type')}
-                />
+            />
+            <div>
                 <Button variant="subtle" onClick={resetHandler}>
                     Сбросить фильтр
                 </Button>
                 <Button variant="light" type='submit'>
                     Найти
                 </Button>
-            </form>
-        </Flex>
+            </div>
+        </form>
     );
 };
 
