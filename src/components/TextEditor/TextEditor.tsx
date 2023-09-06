f47c301bf4f0e3d93a929108a03ba14186a47f69
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-07 00:10:31 +0300

    refactor: formatting

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -1,20 +1,18 @@
-// @ts-ignore
-import { RichTextEditor, Link } from '@mantine/tiptap';
-import { useEditor } from '@tiptap/react';
+import React from "react";
+import {RichTextEditor, Link} from '@mantine/tiptap';
+import {useEditor} from '@tiptap/react';
 import Highlight from '@tiptap/extension-highlight';
 import StarterKit from '@tiptap/starter-kit';
 import Underline from '@tiptap/extension-underline';
 import TextAlign from '@tiptap/extension-text-align';
 import Superscript from '@tiptap/extension-superscript';
 import SubScript from '@tiptap/extension-subscript';
-import {UseFormReturnType } from '@mantine/form'
-import s from "./style.module.css";
-type TextEditorPropsType = {
-    name: string
-    form: UseFormReturnType<any>
-}
-const TextEditor = ({name, form}:TextEditorPropsType) => {
+import {UseFormReturnType} from '@mantine/form'
+import {useStyles} from "./styles";
 
+const TextEditor = ({name, form}: TextEditorPropsType) => {
+
+    const {classes} = useStyles()
     const editor = useEditor({
         extensions: [
             StarterKit,
@@ -23,10 +21,10 @@ const TextEditor = ({name, form}:TextEditorPropsType) => {
             Superscript,
             SubScript,
             Highlight,
-            TextAlign.configure({ types: ['heading', 'paragraph'] }),
+            TextAlign.configure({types: ['heading', 'paragraph']}),
         ],
         content: form.values[name],
-        onUpdate({ editor }) {
+        onUpdate({editor}) {
             form.setFieldValue(name, editor.getHTML());
         },
     });
@@ -34,44 +32,44 @@ const TextEditor = ({name, form}:TextEditorPropsType) => {
 
     return (
         // @ts-ignore
-        <RichTextEditor editor={editor} classNames={{ProseMirror:{height: '500px'}}} >
+        <RichTextEditor editor={editor} classNames={{ProseMirror: classes.mirror, root: classes.root}}>
             <RichTextEditor.Toolbar>
                 <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.Bold />
-                    <RichTextEditor.Italic />
-                    <RichTextEditor.Underline />
-                    <RichTextEditor.Strikethrough />
-                    <RichTextEditor.ClearFormatting />
-                    <RichTextEditor.Highlight />
-                    <RichTextEditor.Code />
+                    <RichTextEditor.Bold/>
+                    <RichTextEditor.Italic/>
+                    <RichTextEditor.Underline/>
+                    <RichTextEditor.Strikethrough/>
+                    <RichTextEditor.ClearFormatting/>
+                    <RichTextEditor.Highlight/>
+                    <RichTextEditor.Code/>
                 </RichTextEditor.ControlsGroup>
 
                 <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.H1 />
-                    <RichTextEditor.H2 />
-                    <RichTextEditor.H3 />
-                    <RichTextEditor.H4 />
+                    <RichTextEditor.H1/>
+                    <RichTextEditor.H2/>
+                    <RichTextEditor.H3/>
+                    <RichTextEditor.H4/>
                 </RichTextEditor.ControlsGroup>
 
                 <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.Blockquote />
-                    <RichTextEditor.Hr />
-                    <RichTextEditor.BulletList />
-                    <RichTextEditor.OrderedList />
-                    <RichTextEditor.Subscript />
-                    <RichTextEditor.Superscript />
+                    <RichTextEditor.Blockquote/>
+                    <RichTextEditor.Hr/>
+                    <RichTextEditor.BulletList/>
+                    <RichTextEditor.OrderedList/>
+                    <RichTextEditor.Subscript/>
+                    <RichTextEditor.Superscript/>
                 </RichTextEditor.ControlsGroup>
 
                 <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.Link />
-                    <RichTextEditor.Unlink />
+                    <RichTextEditor.Link/>
+                    <RichTextEditor.Unlink/>
                 </RichTextEditor.ControlsGroup>
 
                 <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.AlignLeft />
-                    <RichTextEditor.AlignCenter />
-                    <RichTextEditor.AlignJustify />
-                    <RichTextEditor.AlignRight />
+                    <RichTextEditor.AlignLeft/>
+                    <RichTextEditor.AlignCenter/>
+                    <RichTextEditor.AlignJustify/>
+                    <RichTextEditor.AlignRight/>
                 </RichTextEditor.ControlsGroup>
             </RichTextEditor.Toolbar>
             <RichTextEditor.Content/>
@@ -80,4 +78,9 @@ const TextEditor = ({name, form}:TextEditorPropsType) => {
     );
 }
 
-export default TextEditor
+export default React.memo(TextEditor)
+
+type TextEditorPropsType = {
+    name: string
+    form: UseFormReturnType<any>
+}
