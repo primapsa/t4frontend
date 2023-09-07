79879ff6504ac32bf3ffa7f3fd7ef9c4a4e09601
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-07 14:10:46 +0300

    feat: add promocodes component completely

--- src/features/Admin/Promocodes/Promocodes.tsx
+++ src/features/Admin/Promocodes/Promocodes.tsx
@@ -1,42 +1,76 @@
-import React, {useEffect, useState} from 'react';
-import {Container, Group, Header, Button, Flex, Pagination, Paper, Text, Center} from '@mantine/core';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Button, Center, Flex, Modal} from '@mantine/core';
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatchType, RootStateType} from "../../../redux/store";
-import {AppStatus} from "../../../redux/appReducer";
 import {PromocodesType} from "../../../api/api";
-import {fetchPromocodes} from "../../../redux/promocodesReducer";
+import {deletePromocode, fetchPromocodes} from "../../../redux/promocodesReducer";
+import ActionBar from "../../../components/ActionBar/ActionBar";
+import PromocodeForm, {PromocodeInitValueType} from "../../../components/Promocode/PromocodeForm";
+import Promocode from "../../../components/Promocode/Promocode";
+import {useStyles} from "./styles";
+import EmptyStateWithLoader from "../../../components/Empty/EmptyStateWithLoader";
+import {AppStatus} from "../../../redux/appReducer";
+import {getPromocodes, getStatus} from "../../../selectors/selectors";
 
 const Promocodes = () => {
-    const appStatus = useSelector<RootStateType, AppStatus>(state => state.app.status)
-    const promocodes = useSelector<RootStateType, PromocodesType[]>(state => state.promocode.list)
-    const [isFormVisible, setIsFormVisible] = useState<boolean>(false)
-    const [isPageReady, setIsPageReady] = useState<boolean>(false)
-    const list = promocodes.map( p => <PromocodeItem date={p.date} title={p.title} discount={p.discount} /> )
+
+    const {classes} = useStyles()
+    const promocodes = useSelector<RootStateType, PromocodesType[]>(getPromocodes)
+    const status = useSelector<RootStateType, AppStatus>(getStatus)
+
+    const [isModal, setIsModal] = useState<boolean>(false)
+    const [itemEdit, setItemEdit] = useState<PromocodeInitValueType>(undefined)
     const dispatch = useDispatch()
+
+    const deleteItemHandler = useCallback((id: number) => {
+        dispatch<AppDispatchType>(deletePromocode(id))
+    }, [promocodes])
+
+    const editItemHandler = useCallback((pId: number) => {
+        if (promocodes) {
+            const promocode = promocodes.find(({id}) => id === pId)
+            if (promocode) {
+                setItemEdit(promocode)
+                setIsModal(true)
+            }
+        }
+    }, [promocodes])
+
+    const list = promocodes.map(p =>
+        <Flex key={p.id} className={classes.promocode}>
+            <Promocode date={p.date} title={p.title} discount={p.discount}/>
+            <ActionBar id={p.id} del={deleteItemHandler} edit={editItemHandler}/>
+        </Flex>
+    )
+
     useEffect(() => {
         dispatch<AppDispatchType>(fetchPromocodes())
-    },[])
+    }, [])
+
+    const onModalCloseHandler = useCallback(() => setIsModal(false), [])
+    const onClickHandler = useCallback(() => {
+        setItemEdit(undefined)
+        setIsModal(true)
+    }, [])
+    const closeModal = useCallback(() => setIsModal(false), [])
+
+
     return (
-        <Center>
-            {list}
-        </Center>
+        <EmptyStateWithLoader isEmpty={!promocodes.length} status={status}>
+            <Center>
+                <Flex className={classes.wrapper}>
+                    <Button className={classes.button} onClick={onClickHandler}>Добавить промокод</Button>
+                    <Modal opened={isModal} onClose={closeModal}>
+                        <PromocodeForm initValues={itemEdit} onClose={onModalCloseHandler}/>
+                    </Modal>
+                    <Flex className={classes.promocodes}>
+                        {list}
+                    </Flex>
+                </Flex>
+            </Center>
+        </EmptyStateWithLoader>
     );
 };
 
-const PromocodeItem = ({title, discount,date, editMode = false}: PromocodeItemType) => {
- return (
-     <Paper style={{margin: '10px'}}>
-         <Text fw={700}>{discount}%</Text>
-         <Text fw={500}>{title}</Text>
-         <Text fw={500}>до {date}</Text>
-     </Paper>
- )
-}
-type PromocodeItemType = {
-    title: string
-    discount: number
-    date: string
-    editMode?: boolean
-}
 
 export default Promocodes;
