db2ebc305b19591ec42c0b4599412b2f35dc119b
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-07 14:29:09 +0300

    refactor: paypal provider wrap

--- src/features/User/CartPage/CartPage.tsx
+++ src/features/User/CartPage/CartPage.tsx
@@ -1,30 +1,15 @@
 import React, {useMemo} from 'react';
 import Cart from "../../../components/Cart/Cart";
-import {useSelector} from "react-redux";
-import {CartConcertsType} from "../../../api/api";
-import {RootStateType} from "../../../redux/store";
-import {AuthUserType} from "../../../redux/authReducer";
-import {AppStatus} from "../../../redux/appReducer";
+import {PayPalScriptProvider} from "@paypal/react-paypal-js";
 
 const CartPage = () => {
-    const purchases = useSelector<RootStateType, CartConcertsType[]>(state => state.cart.list)
-    const user = useSelector<RootStateType, AuthUserType>(state => state.auth.user)
-    const status = useSelector<RootStateType, AppStatus>(state => state.app.status)
-    const memoPurchases = useMemo(() => purchases, [purchases])
-    // const user = useSelector<RootStateType, AuthUserType>(state => state.auth.user)
 
-    // if(!user.user_id){
-    //     return <EmptyState/>
-    // }
+    const paypalOptions = useMemo(() => ({clientId: process.env.REACT_APP_PAYPAL_CLIENT_ID as string}), [])
+
     return (
-        <>
-
-            {
-                !!user.id &&
-                <Cart purchases={memoPurchases} userId={user.id} status={status}></Cart>
-            }
-
-        </>
+        <PayPalScriptProvider options={paypalOptions}>
+            <Cart/>
+        </PayPalScriptProvider>
     );
 };
 
