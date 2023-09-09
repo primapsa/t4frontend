d355460ffddeb9461935e86966939f05e97ecfe5
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-09 16:37:32 +0300

    feat: add typed dispatch

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,19 +5,30 @@ import filterReducer from "./filterReducer";
 import concertReducer from "./concertReducer";
 import appReducer from "./appReducer";
 import promocodesReducer from "./promocodesReducer";
+import cartReducer from "./cartReducer";
+import authReducer from "./authReducer";
+import { useDispatch } from 'react-redux'
+
 export const rootReducer = combineReducers({
     concerts: concertsReducer,
     filter: filterReducer,
     concert: concertReducer,
     app: appReducer,
-    promocode: promocodesReducer
+    promocode: promocodesReducer,
+    cart: cartReducer,
+    auth: authReducer
 })
+
 export const store = configureStore({
     reducer: rootReducer,
     middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
 })
 
+export const useAppDispatch: () => DispatchType = useDispatch
+
 export type RootStateType = ReturnType<typeof store.getState>
 export type AppDispatchType = typeof store.dispatch | any
+export type DispatchType = typeof store.dispatch
+
 // @ts-ignore
 window.store = store
