495ffe00647300994ecf3754a538b8c4bc46bbfa
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-12 23:20:08 +0300

    feat: add new functionality

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,17 +1,11 @@
-import {authAPI, CartAddType, ConcertsType} from "../api/api";
+import {authAPI, CartAddType} from "../api/api";
 import {AuthInitialType, AuthUserType, LoginType} from "../redux/authReducer";
 import {AxiosError, AxiosInstance, InternalAxiosRequestConfig} from "axios/index";
 import {HTTP_STATUSES} from "../const/htttpStatus";
-import {MEDIA} from "../const/media";
 import {STATUS} from "../const/statuses";
 import {addAppStatus, addAppStatusNotification, AppStatus} from "../redux/appReducer";
 import {MESSAGE} from "../const/messages";
-import {useDispatch} from "react-redux";
 
-export const cordinatesCallback = () => (c: ConcertsType) => ({
-    lat: parseFloat(c.latitude),
-    lng: parseFloat(c.longitude)
-})
 
 export const makeQuery = (param: { query: string, type: number, ids: string }, page: number, count: number) => {
     const queryString = []
@@ -34,19 +28,21 @@ export const getAuthToken = (): string => {
     const header = localStorage.getItem('token')
     return header ? `Bearer ${header}` : ''
 }
+
 export const getRefreshToken = (): string | null => {
     return localStorage.getItem('refresh')
 }
+
 export const makePayload = (concertId: number, price: number, userId: number): CartAddType => {
     return {concertId, price, userId, count: 1, promocodeId: null}
 }
 
-export const authPersistPut = (data: {is_staff: boolean}) => {
+export const authPersistPut = (data: { is_staff: boolean }) => {
     const storaged = {isAuth: true, isStaff: data.is_staff, user: data, type: 'login' as LoginType}
     localStorage.setItem('auth', JSON.stringify(storaged))
 }
 
-export const setTokens = (token: {access: string, refresh: string}) => {
+export const setTokens = (token: { access: string, refresh: string }) => {
     localStorage.setItem('token', token.access)
     localStorage.setItem('refresh', token.refresh)
 }
@@ -54,7 +50,7 @@ export const setTokens = (token: {access: string, refresh: string}) => {
 export const authPersistGet = (): AuthInitialType => {
     const persist = localStorage.getItem('auth')
 
-    return persist ? JSON.parse(persist) : {}
+    return persist ? JSON.parse(persist) : null
 }
 
 const nullFormat = (wn: number) => wn < 10 ? `0${wn}` : wn
@@ -77,7 +73,7 @@ export const discountFormat = (discount: number) => {
     return (Math.round(discount * 100) / 100).toFixed(2)
 }
 
-export const addRequestHeader = (config: InternalAxiosRequestConfig<any>) => {
+export const addRequestHeader =  (config: InternalAxiosRequestConfig<any>) => {
     const token = getAuthToken()
     if (token) {
         config.headers.Authorization = token
@@ -102,7 +98,7 @@ export const refreshExpiredToken = (axiosInstance: AxiosInstance) => async (erro
                     return axiosInstance(config)
                 }
             } catch (err) {
-                localStorage.clear()
+               localStorage.clear()
             }
         }
     }
@@ -114,26 +110,35 @@ export const fetchImagebyUrl = async (url: string) => {
     const buffer = await data.arrayBuffer();
     return new Blob([buffer], {type: "image/png"});
 }
+
 export const generateImageFromUrl = async (url: string, name: string) => {
     const blob = await fetchImagebyUrl(url)
     const file = await new File([blob], name)
     return file
 }
 
-export const handleThunkError = (error: Error, thunkAPI:  any) => {
-
+export const handleThunkError = (error: Error, thunkAPI: any) => {
+    thunkAPI.dispatch(addAppStatus(STATUS.IDLE))
     const notification = {status: STATUS.ERROR, message: error.message}
-    thunkAPI.dispatch(addAppStatusNotification(notification))
-    return thunkAPI.rejectWithValue(error.message)
-}
-export const handleUncaughtError = (thunkAPI:  any) => {
 
+    if (error.message === 'Network Error') {
+        thunkAPI.dispatch(addAppStatus(STATUS.OFFLINE))
+    } else {
+        thunkAPI.dispatch(addAppStatusNotification(notification))
+    }
+
+    return thunkAPI.rejectWithValue(error)
+}
+
+export const handleUncaughtError = (thunkAPI: any) => {
+
+    thunkAPI.dispatch(addAppStatus(STATUS.IDLE))
     const notification = {status: STATUS.ERROR, message: MESSAGE.UNCAUGHT}
     thunkAPI.dispatch(addAppStatusNotification(notification))
     return thunkAPI.rejectWithValue(MESSAGE.UNCAUGHT)
 }
 
-export const asyncThunkActionWithLoading =  async (action: Function, actionParam: any, thunkAPI: any ) => {
+export const asyncThunkActionWithLoading = async (action: Function, actionParam: any, thunkAPI: any) => {
     thunkAPI.dispatch(addAppStatus(STATUS.LOADING))
     try {
         const response = await action(actionParam)
@@ -143,7 +148,7 @@ export const asyncThunkActionWithLoading =  async (action: Function, actionParam
         }
         return handleUncaughtError(thunkAPI)
     } catch (error) {
-        return handleThunkError(error as Error, thunkAPI )
+        return handleThunkError(error as Error, thunkAPI)
     }
 }
 
