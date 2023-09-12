40ca79c7884f4032011960fdf3d701e01470f6ee
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-12 23:27:56 +0300

    feat: add selectors

--- src/selectors/selectors.ts
+++ src/selectors/selectors.ts
@@ -1,4 +1,6 @@
 import {RootStateType} from "../redux/store";
+import {createSelector} from "@reduxjs/toolkit";
+import {STATUS} from "../const/statuses";
 
 export const getNotice = (state:RootStateType) => state.app.notification
 export const getConcerts = (state:RootStateType) => state.concerts.list
@@ -6,6 +8,7 @@ export const getSingerVoice = (state:RootStateType) => state.concerts.singerVoic
 export const getPopup = (state:RootStateType) => state.app.popup
 export const getCart = (state:RootStateType) => state.cart.list
 export const getUser = (state:RootStateType) => state.auth.user
+export const getUserId = (state:RootStateType) => state.auth.userId
 export const getStatus = (state:RootStateType) => state.app.status
 export const getConcertType = (state:RootStateType) => state.concerts.type
 export const getFilterQuery = (state:RootStateType) => state.filter.query
@@ -15,3 +18,11 @@ export const getIsStaff = (state:RootStateType) => state.auth.isStaff
 export const getPromocodes = (state:RootStateType) => state.promocode.list
 export const getTotal = (state:RootStateType) => state.concerts.total
 export const getPage = (state:RootStateType) => state.concerts.page
+export const getTotalPromocode = (state:RootStateType) => state.promocode.total
+export const getPagePromocode = (state:RootStateType) => state.promocode.page
+
+export const getMemoOfflineStatus = createSelector(getStatus,
+    (status) => status === STATUS.OFFLINE ? status : undefined)
+
+export const getMemoLoadingStatus = createSelector(getStatus,
+    (status) => status === STATUS.LOADING ? status : undefined)
