de02ef15fdae5544ccf214e5c8bb019bb33af541
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-13 14:38:05 +0300

    refactor: use prod backend urls

--- src/const/media.ts
+++ src/const/media.ts
@@ -1,5 +1,5 @@
 export const MEDIA = {
-    URL2: 'http://127.0.0.1:8000/media/',
+    URL_: 'http://127.0.0.1:8000/media/',
     URL: 'https://t4uback.onrender.com/media/'
 
 }
--- src/const/restApi.ts
+++ src/const/restApi.ts
@@ -1,4 +1,4 @@
 export const REST_API = {
-    "BASE_URL": "http://127.0.0.1:8000/api/",
-    "BASE_URL_": "https://t4uback.onrender.com/api/"
+    "BASE_URL_": "http://127.0.0.1:8000/api/",
+    "BASE_URL": "https://t4uback.onrender.com/api/"
 }
